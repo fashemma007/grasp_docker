05a8be7a81dda1081d2edc0d9cde65c1284d0705
fashemma007 <fasogbaemmanuel@gmail.com>
2021-11-23 02:56:15 +0100

    index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get("/", (req, res) =>
         },
         {
             name: "Jake",
-            email: "jam@gmail.com",
+            email: "jake@gmail.com",
         },
         {
             name: "Maria",
